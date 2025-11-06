96d4313bc19d3922f3fa7fbc7eeb8796fb5ffce5
王时青 <sophomore@duck.com>
2025-11-06 19:18:31 +0800

    feat:integrate shaderc

--- main.cpp
+++ main.cpp
@@ -1,93 +1,103 @@
-#define SDL_MAIN_USE_CALLBACKS
-#include <SDL3/SDL_main.h>
-#include <SDL3/SDL.h>
+#include "shaderc/shaderc.hpp"
+#include <iostream>
 
-SDL_Window* window;
-SDL_GPUDevice* device;
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+SDL_Window *window;
+SDL_GPUDevice *device;
 
 // the vertex input layout
-struct Vertex
-{
-    float x, y, z;      //vec3 position
-    float r, g, b, a;   //vec4 color
+struct Vertex {
+  float x, y, z;    // vec3 position
+  float r, g, b, a; // vec4 color
 };
 
 // a list of vertices
-static Vertex vertices[]
-{
-    {0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f},     // top vertex
-    {-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f},   // bottom left vertex
-    {0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f}     // bottom right vertex
+static Vertex vertices[]{
+    {0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f},   // top vertex
+    {-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f}, // bottom left vertex
+    {0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f}   // bottom right vertex
 };
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
-{
-    // create a window
-    window = SDL_CreateWindow("Hello, Triangle!", 960, 540, SDL_WINDOW_RESIZABLE);
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
 
-    // create the device
-    device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
-    SDL_ClaimWindowForGPUDevice(device, window);
-    SDL_GPUShaderCreateInfo vertexInfo{};
-    SDL_CreateGPUShader(device,&vertexInfo);
+  shaderc::Compiler compiler{};
+  shaderc::CompileOptions options{};
+  options.SetTargetEnvironment(shaderc_target_env_vulkan, 0);
+  auto result = compiler.CompileGlslToSpv(
+      std::string{""}, shaderc_glsl_vertex_shader, "test.glsl", options);
 
-    return SDL_APP_CONTINUE;
+  if (result.GetCompilationStatus() != shaderc_compilation_status_success) {
+    std::cerr << "[shaderc] compile error in " << debug_name << ":\n"
+              << result.GetErrorMessage() << std::endl;
+  }
+
+  // create a window
+  window = SDL_CreateWindow("Hello, Triangle!", 960, 540, SDL_WINDOW_RESIZABLE);
+
+  // create the device
+  device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
+  SDL_ClaimWindowForGPUDevice(device, window);
+  SDL_GPUShaderCreateInfo vertexInfo{};
+  SDL_CreateGPUShader(device, &vertexInfo);
+
+  return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    // acquire the command buffer
-    SDL_GPUCommandBuffer* commandBuffer = SDL_AcquireGPUCommandBuffer(device);
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  // acquire the command buffer
+  SDL_GPUCommandBuffer *commandBuffer = SDL_AcquireGPUCommandBuffer(device);
 
-    // get the swapchain texture
-    SDL_GPUTexture* swapchainTexture;
-    Uint32 width, height;
-    SDL_WaitAndAcquireGPUSwapchainTexture(commandBuffer, window, &swapchainTexture, &width, &height);
+  // get the swapchain texture
+  SDL_GPUTexture *swapchainTexture;
+  Uint32 width, height;
+  SDL_WaitAndAcquireGPUSwapchainTexture(commandBuffer, window,
+                                        &swapchainTexture, &width, &height);
 
-    // end the frame early if a swapchain texture is not available
-    if (swapchainTexture == NULL)
-    {
-        // you must always submit the command buffer
-        SDL_SubmitGPUCommandBuffer(commandBuffer);
-        return SDL_APP_CONTINUE;
-    }
-
-    // create the color target
-    SDL_GPUColorTargetInfo colorTargetInfo{};
-    colorTargetInfo.clear_color = {240/255.0f, 240/255.0f, 240/255.0f, 255/255.0f};
-    colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
-    colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
-    colorTargetInfo.texture = swapchainTexture;
-
-    // begin a render pass
-    SDL_GPURenderPass* renderPass = SDL_BeginGPURenderPass(commandBuffer, &colorTargetInfo, 1, NULL);
-
-    // draw something
-
-    // end the render pass
-    SDL_EndGPURenderPass(renderPass);
-
-    // submit the command buffer
+  // end the frame early if a swapchain texture is not available
+  if (swapchainTexture == NULL) {
+    // you must always submit the command buffer
     SDL_SubmitGPUCommandBuffer(commandBuffer);
-
     return SDL_APP_CONTINUE;
+  }
+
+  // create the color target
+  SDL_GPUColorTargetInfo colorTargetInfo{};
+  colorTargetInfo.clear_color = {240 / 255.0f, 240 / 255.0f, 240 / 255.0f,
+                                 255 / 255.0f};
+  colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
+  colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
+  colorTargetInfo.texture = swapchainTexture;
+
+  // begin a render pass
+  SDL_GPURenderPass *renderPass =
+      SDL_BeginGPURenderPass(commandBuffer, &colorTargetInfo, 1, NULL);
+
+  // draw something
+
+  // end the render pass
+  SDL_EndGPURenderPass(renderPass);
+
+  // submit the command buffer
+  SDL_SubmitGPUCommandBuffer(commandBuffer);
+
+  return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-    // close the window on request
-    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
-    {
-        return SDL_APP_SUCCESS;
-    }
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  // close the window on request
+  if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
+    return SDL_APP_SUCCESS;
+  }
 
-    return SDL_APP_CONTINUE;
+  return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
-    // destroy the GPU device
-    SDL_DestroyGPUDevice(device);
-    // destroy the window
-    SDL_DestroyWindow(window);
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+  // destroy the GPU device
+  SDL_DestroyGPUDevice(device);
+  // destroy the window
+  SDL_DestroyWindow(window);
 }
