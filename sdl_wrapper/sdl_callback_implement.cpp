46e77a768fe5cbc7758e9434f7dfd48628fca229
Sophomore <17792626+WSQS@users.noreply.github.com>
2025-11-12 20:20:21 +0800

    fix:call sdl init before app init

--- sdl_wrapper/sdl_callback_implement.cpp
+++ sdl_wrapper/sdl_callback_implement.cpp
@@ -9,6 +9,7 @@ import sdl_wrapper;
 extern sopho::App *create_app();
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+  SDL_Init(SDL_INIT_VIDEO);
   auto app = create_app();
   if (!app)
     return SDL_APP_FAILURE;
