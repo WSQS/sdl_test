2a99d22bf42b0f72aeb253f4073e442e91286072
Sophomore <sophomore@duck.com>
2025-11-14 12:48:00 +0800

    fix: try to compile on clang18

---
+++ sdl_wrapper/sdl_wrapper.gpu.cpp
@@ -0,0 +1,11 @@
+﻿//
+// Created by wsqsy on 11/14/2025.
+//
+module;
+module sdl_wrapper;
+import :gpu;
+import :pipeline;
+namespace sopho
+{
+    PipelineWrapper GpuWrapper::create_pipeline() { return PipelineWrapper{shared_from_this()}; }
+}
