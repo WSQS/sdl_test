b695abd20a0a4225b7406e39e0f64cd8e7f1849d
Sophomore <17792626+WSQS@users.noreply.github.com>
2025-11-15 19:03:42 +0800

    fix: set uniform to 1 to avoid crash

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ layout (location = 0) in vec3 a_position;
 layout (location = 1) in vec4 a_color;
 layout (location = 0) out vec4 v_color;
 
-layout(std140, set = 0, binding = 0) uniform Camera
+layout(std140, set = 1, binding = 0) uniform Camera
 {
     mat4 uView;
 };
