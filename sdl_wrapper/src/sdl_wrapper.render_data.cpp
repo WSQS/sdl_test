33b957a5b6b9ea2f7d7abfad60095ac5289d5ac1
Sophomore <17792626+WSQS@users.noreply.github.com>
2025-11-27 22:40:03 +0800

    feat: change uv for squire

--- sdl_wrapper/src/sdl_wrapper.render_data.cpp
+++ sdl_wrapper/src/sdl_wrapper.render_data.cpp
@@ -53,22 +53,38 @@ namespace sopho
         vertex_ptr[36] = -0.5;
         vertex_ptr[37] = -0.5;
 
-        vertex_ptr[3] = 1.0;
-        vertex_ptr[4] = 1.0;
-        vertex_ptr[8] = 1.0;
-        vertex_ptr[9] = 0;
-        vertex_ptr[13] = 0;
-        vertex_ptr[14] = 1.0;
-        vertex_ptr[18] = 0;
-        vertex_ptr[19] = 0;
-        vertex_ptr[23] = 0;
-        vertex_ptr[24] = 1.0;
-        vertex_ptr[28] = 0;
-        vertex_ptr[29] = 0;
-        vertex_ptr[33] = 1;
-        vertex_ptr[34] = 1.0;
-        vertex_ptr[38] = 1;
-        vertex_ptr[39] = 0;
+        // v0 uv
+        vertex_ptr[3]  = 0.0f;
+        vertex_ptr[4]  = 0.0f;
+
+        // v1 uv
+        vertex_ptr[8]  = 1.0f;
+        vertex_ptr[9]  = 0.0f;
+
+        // v2 uv
+        vertex_ptr[13] = 0.0f;
+        vertex_ptr[14] = 1.0f;
+
+        // v3 uv
+        vertex_ptr[18] = 1.0f;
+        vertex_ptr[19] = 1.0f;
+
+        // v4 uv
+        vertex_ptr[23] = 1.0f;
+        vertex_ptr[24] = 1.0f;
+
+        // v5 uv
+        vertex_ptr[28] = 0.0f;
+        vertex_ptr[29] = 1.0f;
+
+        // v6 uv
+        vertex_ptr[33] = 1.0f;
+        vertex_ptr[34] = 0.0f;
+
+        // v7 uv
+        vertex_ptr[38] = 0.0f;
+        vertex_ptr[39] = 0.0f;
+
         int* index_ptr = reinterpret_cast<int*>(index_buffer.value().cpu_buffer());
         index_ptr[0] = 0;
         index_ptr[1] = 1;
@@ -88,8 +104,24 @@ namespace sopho
         index_ptr[15] = 2;
         index_ptr[16] = 4;
         index_ptr[17] = 6;
-        index_ptr[18] = 0;
-        index_ptr[19] = 0;
+        index_ptr[18] = 2;
+        index_ptr[19] = 3;
+        index_ptr[20] = 6;
+        index_ptr[21] = 3;
+        index_ptr[22] = 6;
+        index_ptr[23] = 7;
+        index_ptr[24] = 1;
+        index_ptr[25] = 3;
+        index_ptr[26] = 5;
+        index_ptr[27] = 3;
+        index_ptr[28] = 5;
+        index_ptr[29] = 7;
+        index_ptr[30] = 4;
+        index_ptr[31] = 5;
+        index_ptr[32] = 6;
+        index_ptr[33] = 5;
+        index_ptr[34] = 6;
+        index_ptr[35] = 7;
         return std::make_shared<RenderDataImpl>(std::move(vertex_buffer.value()), std::move(index_buffer.value()),
                                                 layout, vertex_count, index_count);
     }
